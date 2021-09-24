0c860f25f6979118ad9d46530cab4a0a50ac6da7
nateshim <nateshim@berkeley.edu>
2021-09-24 15:14:07 -0700

    trying something out

--- client/src/components/Move.jsx
+++ client/src/components/Move.jsx
@@ -3,7 +3,9 @@ import {pieceSVG} from '../services';
 function Move(props) {
   return (
     <div className="Move">
-      {pieceSVG[props.move.piece.color.concat(props.move.piece.type.toUpperCase())]}
+      <div className="Piece">
+        {pieceSVG[props.move.piece.color.concat(props.move.piece.type.toUpperCase())]}
+      </div>
       <p>From: {props.move.from}</p>
       <p>To: {props.move.to}</p>
     </div>
